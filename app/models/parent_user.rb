aa7205017f53eccb066f95889c87af98e18b467d
annelaure ozzola <ozzolaa@gmail.com>
2022-11-29 14:12:30 +0100

    création de la table parent_users

---
+++ app/models/parent_user.rb
@@ -0,0 +1,9 @@
+class ParentUser < ApplicationRecord
+
+  validates :first_name, :last_name, :email, presence: true
+
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+end
